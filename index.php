c051c9084655a35d9619761e68cc04a19681b55c
Hierya <ira@ira.design>
2024-01-27 16:40:11 -0500

    Initial setup

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Pies</title>
+    <meta name="description" content="Hey this is a website for Ira Bronson, a designer in New York City, remotely creating usable experiences for humans." />
+    <link rel="author" href="/humans.txt" type="text/plain" />
+    <!-- ========== Allow to conform to mobile screens -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ========== Touch and Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=3">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=3">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=3">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="shortcut icon" href="/favicon.ico?v=3">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="theme-color" content="#000000">
+    <!-- ========== Facebook and Twitter Share Meta -->
+    <meta property="og:url" content="https://ira.design">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Pies">
+    <meta property="og:description" content="Hey this is a website for Ira Bronson, a designer in New York City, remotely creating usable experiences for humans.">
+    <meta property="og:image" content="https://ira.design/img/facebook-og-v3.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:image" content="https://ira.design/img/twitter-v3.png">
+    <!-- ========== CSS -->
+    <link rel="stylesheet" href="/css/main.css?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/css/main.css'); ?>">    
+    <!-- ========== JS deferred -->
+    <script defer src="/js/plugins.js"></script>
+    <script defer src="/js/main.js?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/js/main.js'); ?>"></script>
+  </head>
+  <body>
+
+    <!-- ========== Page container -->
+    <div id="page" class="">
+	    
+      <!-- Header -->
+      
+      <main class="">
+        
+        <!-- Content container -->
+        <div id="content-container">
+
+          <!-- ==========  -->
+          <section class="">
+            <div class="col-8">
+              <h1 class="">Pies</h1>
+              <p class="">Asadsdf sdfsd gdfgfdg</p>
+            </div>
+          </section> <!--  -->
+
+          
+        </div> <!-- #content-container -->
+          
+        <!-- Footer -->
+
+  
+      </main> <!--  -->
+
+    </div> <!-- #page -->
+    
+  </body>
+</html>
